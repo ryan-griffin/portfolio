053d6505379642fe7d3fff8754163d451faa0ad6
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2021-09-28 18:34:32 -0400

    Add files via upload

--- Portfolio_Website/javascript.js
+++ Portfolio_Website/javascript.js
@@ -1,4 +1,23 @@
-function theme() {
-    document.body.classList.toggle("dark_theme");
-    document.body.classList.toggle("light_theme");
+// function to set a given theme/color-scheme
+function set_theme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
 }
+
+// function to toggle between light and dark theme
+function toggle_theme() {
+    if (localStorage.getItem('theme') === 'dark_theme') {
+        set_theme('light_theme');
+    } else {
+        set_theme('dark_theme');
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'dark_theme') {
+        set_theme('dark_theme');
+    } else {
+        set_theme('light_theme');
+    }
+})();
