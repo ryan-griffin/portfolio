53899c19d472e388663ea1b01641cc054c0e728b
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2021-10-07 17:34:05 -0400

    Update javascript.js

--- Portfolio_Website/javascript.js
+++ Portfolio_Website/javascript.js
@@ -14,10 +14,8 @@ function toggle_theme() {
 }
 
 // Immediately invoked function to set the theme on initial load
-(function () {
-    if (localStorage.getItem('theme') === 'light_theme') {
-        set_theme('light_theme');
-    } else {
-        set_theme('dark_theme');
-    }
-})();
+if (localStorage.getItem('theme') === 'light_theme') {
+    set_theme('light_theme');
+} else {
+    set_theme('dark_theme');
+}
