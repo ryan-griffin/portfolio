de21fdc1ccf8a624f5c657fa969242799ff7c546
ryan-griffin <ryan-griffin@users.noreply.github.com>
2021-11-23 18:53:10 -0500

    update

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// function to set theme
+function set_theme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggle_theme() {
+    if (localStorage.getItem('theme') === 'dark_theme') {
+        set_theme('light_theme');
+    } else {
+        set_theme('dark_theme');
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+if (localStorage.getItem('theme') === 'light_theme') {
+    set_theme('light_theme');
+} else {
+    set_theme('dark_theme');
+}
