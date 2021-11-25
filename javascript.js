dedd9754cb03f26f551f1155235527f46a3cee79
ryan-griffin <ryan-griffin@users.noreply.github.com>
2021-11-24 23:32:48 -0500

    update

--- javascript.js
+++ javascript.js
@@ -20,21 +20,15 @@ if (localStorage.getItem("theme") === "light_theme") {
     set_theme("dark_theme");
 }
 
-var x = document.getElementById("links");
-var y = document.getElementById("hamburger")
-
+// opens and closes the mobile navigation links
 function expand() {
-    if (x.style.width === "200px" && localStorage.getItem("theme") === "dark_theme") {
+    var x = document.getElementById("links");
+    var y = document.getElementById("hamburger");
+    if (x.style.width === "200px") {
         x.style.width = "0";
-        y.style.backgroundImage = "images/hamburger.png";
-    } else if (x.style.width === "0" && localStorage.getItem("theme") === "dark_theme") {
+        y.style.backgroundImage = "url(images/hamburger.png)";
+    } else {
         x.style.width = "200px";
-        y.style.backgroundImage = "images/close.png";
-    } else if (x.style.width === "200px" && localStorage.getItem("theme") === "light_theme") {
-        x.style.width = "0";
-        y.style.backgroundImage = "images/hamburger_black.png";
-    } else if (x.style.width === "0" && localStorage.getItem("theme") === "light_theme") {
-        x.style.width = "200px";
-        y.style.backgroundImage = "images/close_black.png";
+        y.style.backgroundImage = "url(images/close.png)";
     }
 }
