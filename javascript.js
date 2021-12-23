779ba821609e6e89aa36918d185d926f6de77a64
ryan-griffin <ryan-griffin@users.noreply.github.com>
2021-12-23 17:02:48 -0500

    update

--- javascript.js
+++ javascript.js
@@ -27,3 +27,25 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "flex";
 }
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+function currentSlide2(n) {
+    showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  var y;
+  var slides2 = document.getElementsByClassName("slide2");
+  if (n > slides2.length) {slideIndex2 = 1}    
+  if (n < 1) {slideIndex2 = slides2.length}
+  for (y = 0; y < slides2.length; y++) {
+      slides2[y].style.display = "none";  
+  }
+  slides2[slideIndex2-1].style.display = "flex";
+}
