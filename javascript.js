5fef8eda0490704f52be3f6d25a83e56e5d493d9
ryan-griffin <ryan-griffin@users.noreply.github.com>
2022-01-07 20:21:58 -0500

    update

--- javascript.js
+++ javascript.js
@@ -1,14 +1,34 @@
 const show_hide = () => document.querySelector("nav").classList.toggle("show");
-
 const hide = () => document.querySelector("nav").classList.remove("show");
+
+const about = document.getElementById("about");
+const projects = document.getElementById("projects");
+const certifications = document.getElementById("certifications");
+const resume = document.getElementById("resume");
+const animate_about = () => about.classList.add("animate-about");
+const animate_projects = () => projects.classList.add("animate-projects");
+const animate_certifications = () =>
+    certifications.classList.add("animate-certifications");
+const aniamte_resume = () => resume.classList.add("animate-resume");
+function reanimate(section) {
+    if (section == 1) {
+        about.classList.remove("animate-about");
+        setTimeout(animate_about);
+    } else if (section == 2) {
+        projects.classList.remove("animate-projects");
+        setTimeout(animate_projects);
+    } else if (section == 3) {
+        certifications.classList.remove("animate-certifications");
+        setTimeout(animate_certifications);
+    } else if (section == 4) {
+        resume.classList.remove("animate-resume");
+        setTimeout(aniamte_resume);
+    }
+}
 
 let slideIndex = 1;
 showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides((slideIndex += n));
-}
-
+const plusSlides = (n) => showSlides((slideIndex += n));
 function showSlides(n) {
     let x;
     let slides = document.getElementsByClassName("slide");
@@ -26,11 +46,7 @@ function showSlides(n) {
 
 let slideIndex2 = 1;
 showSlides2(slideIndex2);
-
-function plusSlides2(n) {
-    showSlides2((slideIndex2 += n));
-}
-
+const plusSlides2 = (n) => showSlides2((slideIndex2 += n));
 function showSlides2(n) {
     let y;
     let slides2 = document.getElementsByClassName("slide2");
